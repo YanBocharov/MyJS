8e166117df2cde5215ec51d998c51281f1e95579
Yan <yan.bocharovv@gmail.com>
2022-03-18 14:06:41 +0300

    Start

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+for(let i = 0; i < 2; i++) {
+   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	    b = prompt("Во сколько обойдется?", ''),
+}
+
+// alert(appData.budget / 30);
